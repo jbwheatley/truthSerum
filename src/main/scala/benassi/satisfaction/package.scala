72709cc9e76511c7668222f52c8d3002f524dc06
Jack Wheatley <jackwheatley@Jacks-MBP.fritz.box>
2019-04-10 22:43:42 +0100

    implicits and tests

---
+++ src/main/scala/benassi/satisfaction/package.scala
@@ -0,0 +1,14 @@
+package benassi
+
+package object satisfaction {
+
+  type T = True
+  type F = False
+
+  type ~[L] = Not[L]
+  type &[L1, L2] = And[L1, L2]
+  type |[L1, L2] = ~[~[L1] & ~[L2]]
+  type ->[L1, L2] = ~[L1 & ~[L2]]
+  type <->[L1, L2] = L1 -> L2 & (L2 -> L1)
+
+}
