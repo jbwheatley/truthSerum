ecc7c1b897435286c0d566b0194c7c7a27720a58
jbwheatley <19939655+jbwheatley@users.noreply.github.com>
2019-05-25 11:32:19 +0100

    rename not to ! from ~, reduce number of falseness implicits

--- src/test/scala/truthSerum/satisfaction/SatisfactionTests.scala
+++ src/test/scala/truthSerum/satisfaction/SatisfactionTests.scala
@@ -13,15 +13,15 @@ class SatisfactionTests extends FlatSpec with Matchers {
 
   "satisfiable1" should "compile if the expression has a satisfying interpretation" in {
 
-    type X[A] = ~[~[~[~[A]]]]
+    type X[A] = ![![![![A]]]]
     type Y[A] = A & A
-    type Z[A] = ~[A] | A
+    type Z[A] = ![A] | A
 
     satisfiable1[X]
     satisfiable1[Y]
     satisfiable1[Z]
 
-    type W[A] = A & ~[A]
+    type W[A] = A & ![A]
 
     assertDoesNotCompile("satisfiable1[W]")
   }
@@ -29,14 +29,14 @@ class SatisfactionTests extends FlatSpec with Matchers {
   "satisfiable2" should "compile if the expression has a satisfying interpretation" in {
 
     type X[A, B] = A -> (B -> A)
-    type Y[A, B] = ~[A] & ~[B]
+    type Y[A, B] = ![A] & ![B]
     type Z[A, B] = B <-> A
 
     satisfiable2[X]
     satisfiable2[Y]
     satisfiable2[Z]
 
-    type W[A, B] = ~[A -> (B -> A)]
+    type W[A, B] = ![A -> (B -> A)]
 
     assertDoesNotCompile("satisfiable2[W]")
   }
@@ -44,15 +44,15 @@ class SatisfactionTests extends FlatSpec with Matchers {
   "satisfiable3" should "compile if the expression has a satisfying interpretation" in {
 
     type X[A, B, C] = A -> B -> (B -> C -> (A -> C))
-    type Y[A, B, C] = ~[A] <-> B & ~[C]
-    type Z[A, B, C] = ~[A & ~[A] & C & ~[B] & B & ~[C]]
+    type Y[A, B, C] = ![A] <-> B & ![C]
+    type Z[A, B, C] = ![A & ![A] & C & ![B] & B & ![C]]
 
     satisfiable3[X]
     satisfiable3[Y]
     satisfiable3[Z]
 
-    type W[A, B, C] = ~[A -> (B -> A)]
-    type U[A, B, C] = A & ~[A] & C & ~[B] & B & ~[C]
+    type W[A, B, C] = ![A -> (B -> A)]
+    type U[A, B, C] = A & ![A] & C & ![B] & B & ![C]
 
     assertDoesNotCompile("satisfiable3[W]")
     assertDoesNotCompile("satisfiable3[U]")
